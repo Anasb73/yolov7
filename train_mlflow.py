bd187dc4add6ae9a8de3595e17948a319b84e9ec
Anas Benalla <anas.benalla@asygn.com>
2024-05-15 13:58:50 +0200

    remove progression

--- train_mlflow.py
+++ train_mlflow.py
@@ -348,8 +348,8 @@ def train(hyp, opt, device, tb_writer=None):
                 dataloader.sampler.set_epoch(epoch)
             pbar = enumerate(dataloader)
             logger.info(('\n' + '%10s' * 8) % ('Epoch', 'gpu_mem', 'box', 'obj', 'cls', 'total', 'labels', 'img_size'))
-            if rank in [-1, 0]:
-                pbar = tqdm(pbar, total=nb)  # progress bar
+            # if rank in [-1, 0]:
+            #     pbar = tqdm(pbar, total=nb)  # progress bar
             optimizer.zero_grad()
             for i, (imgs, targets, paths, _) in pbar:  # batch -------------------------------------------------------------
                 ni = i + nb * epoch  # number integrated batches (since train start)
